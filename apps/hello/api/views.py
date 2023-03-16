542d5769d9581af4c8e2a51ce2912cc75b43189d
Santiago Corrales <santi368444110@gmail.com>
2023-03-16 17:02:35 -0500

    wip: add test token create and validate exits

--- apps/hello/api/views.py
+++ apps/hello/api/views.py
@@ -1,10 +1,35 @@
 from rest_framework import views
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 class HelloWorldAPIView(views.APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'message': 'Hello World!'
-        }
-        return Response(data)
+        # Get user
+        user = User.objects.all().first()
+        # Get token
+        token = Token.objects.filter(user=user)
+        # Validate if token exist
+        if token.exists():
+            # If exist, return token
+            token = token.first()
+            data = {
+                'message': 'Not create!',
+                'token': token.key
+            }
+            return Response(data)
+            
+        else:# If not exist, then create new token user
+            token = Token.objects.create(user=user)       
+            data = {
+                'message': 'Create!',
+                'token': token.key
+            }
+            return Response(data)
+    
+
+
+
